bbe274bd90634ed024c9965e951d198a5809c96a
Noah Marquie <noah.marquie@gmail.com>
2026-06-16 10:18:59 -0700

    Clean hexagon creation

--- geometry/hex_geometry.py
+++ geometry/hex_geometry.py
@@ -27,12 +27,40 @@ class Hexagon:
 
         self.triangles = [tri_1, tri_2, tri_3, tri_4, tri_5, tri_6]
 
+        # Put together boundary and inner points from each triangle
+        self.inner_points = np.vstack([
+            tri_1.inner_points,
+            tri_2.inner_points,
+            tri_3.inner_points,
+
+            tri_4.inner_points,
+            tri_5.inner_points,
+            tri_6.inner_points,
+            
+            tri_1.edge2.points, tri_1.edge2.end, 
+            tri_2.edge1.points,
+            tri_3.edge1.points,
+            tri_4.edge3.points,
+            tri_5.edge3.points,
+            tri_6.edge2.points,
+        ])
+
+        self.boundary_points = np.vstack([
+            tri_1.edge1.points, tri_1.edge1.start,
+            tri_2.edge3.points, tri_2.edge3.end,
+            tri_3.edge3.points, tri_3.edge3.end,
+            tri_4.edge2.points, tri_4.edge2.end,
+            tri_5.edge2.points, tri_5.edge2.end,
+            tri_6.edge1.points, tri_6.edge1.start,
+        ])
+
 
     def visualize(self):
-        for tri in self.triangles:
-            tri.visualize()
+        plt.scatter(self.inner_points[:, 0], self.inner_points[:, 1], alpha=0.5)
+        plt.scatter(self.boundary_points[:, 0], self.boundary_points[:, 1], alpha=0.5, c="red")
 
 
-hex = Hexagon(1, step_size = 0.1)
-hex.visualize()
-plt.show()
+if __name__ == "__main__":
+    hex = Hexagon(1, step_size = 0.1)
+    hex.visualize()
+    plt.show()
